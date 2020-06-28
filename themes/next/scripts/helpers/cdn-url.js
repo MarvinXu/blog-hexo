37dc28912bcddddc2cf67c0ca2a201ae41ccbb50
xupeiyuan <xupeiyuan123@qq.com>
2020-06-28 12:17:56 +0800

    google_analytics

--- themes/next/scripts/helpers/cdn-url.js
+++ themes/next/scripts/helpers/cdn-url.js
@@ -4,9 +4,7 @@
 
 hexo.extend.helper.register('cdn_url', function (path) {
   // const { config } = this;
-
   const theme = hexo.theme.config;
-  console.log('cdn:' + theme.cdn)
   if (theme.cdn) {
     path = path.startsWith('/') ? path : '/' + path
     return theme.cdn.base + path
